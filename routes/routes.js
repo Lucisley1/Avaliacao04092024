132b0219cf730daf98f687be2d38ac4a97e73882
Lucisley1 <mlucisley1@gmail.com>
2024-04-09 20:38:27 -0300

    rev1

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,10 @@ const controller = require("../controllers/controller")
 
 const routes = express.Router()
   
-routes.get("/produto",controller.getAllProduto)
-routes.get("/produto/:id",controller.getProdutoById)
-routes.post("produto",controller.createProduto)
-routes.put("/produto/:id",controller.updateProduto)
-routes.delete("/produto/:id",controller.deleteProduto)
+routes.get("/",controller.getAllProduto)
+routes.get("/:id",controller.getProdutoById)
+routes.post("/",controller.createProduto)
+routes.put("/:id",controller.updateProduto)
+routes.delete("/:id",controller.deleteProduto)
 
 module.exports = routes
